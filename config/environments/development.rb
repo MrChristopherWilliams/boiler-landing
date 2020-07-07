5435d90da66921281f579f63297e0ee7300407eb
Chris Williams <chriswilliams14.williams@gmail.com>
2020-07-07 12:55:16 +0100

    Completed boilerplate landing page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << 'c3d1ecdc3314.ngrok.io'
+  config.hosts << 'localhost'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
